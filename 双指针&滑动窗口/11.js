60b42c3ac3e8a8b74c5d31a845524e882769e503
lruler <1204312199@qq.com>
2022-03-10 14:17:25 +0800

    add 11

---
+++ 双指针&滑动窗口/11.js
@@ -0,0 +1,15 @@
+ var maxArea = function (height) {
+     let left = 0
+     let right = height.length - 1
+     let max = 0
+     while (left < right) {
+         curArea = Math.min(height[left], height[right]) * (right - left)
+         max = Math.max(max, curArea)
+         if (height[left] <= height[right]) {
+             left++
+         } else {
+             right--
+         }
+     }
+     return max
+ };
